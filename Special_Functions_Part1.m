1102c58145497e4083779cd10f5489352da2f000
AyhamRatrout <arr90@case.edu>
2022-01-22 18:45:50 -0500

    Second Commit - Finished the rect and sinc functions for part 6

--- Special_Functions_Part1.m
+++ Special_Functions_Part1.m
@@ -2,8 +2,12 @@ t1 = linspace(-10*pi, 10*pi);
 y1 = sinc(t1);
 subplot(2, 1, 1);
 plot(t1, y1);
+xlabel('Time (t)');
+ylabel('sinc(t)');
 
 t2 = linspace(-2, 2);
 y2 = (abs(t2) <= 0.5);
 subplot(2, 1, 2);
 plot(t2, y2);
+xlabel('Time (t)');
+ylabel('rect(t)');
